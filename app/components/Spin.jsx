22e291cdf9fd25f2d5e012343dc6e91049b64f66
Zhe Feng <zhe@jumiduo.com>
2017-01-24 10:26:37 +1100

    Add spin

--- app/components/Spin.jsx
+++ app/components/Spin.jsx
@@ -1,10 +1,38 @@
 // @flow
 import React, { Component } from 'react';
 
+const inner = {
+  borderRadius: '100%',
+  transition: 'all 1000ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+  // position: 'absolute',
+  width: '16px',
+  height: '16px',
+  border: '1px solid #0275d8',
+  borderRightColor: 'transparent',
+  borderLeftColor: 'transparent',
+  transform: 'rotate(0deg)',
+  margin: '0 auto',
+};
+
 class Spin extends Component {
+  state = {
+    rotate: 0,
+  }
+  componentDidMount() {
+    setTimeout(this.setPercentage.bind(this), 100);
+  }
+  setPercentage() {
+    const { rotate } = this.state;
+    return this.setState(
+      { rotate: rotate + 180 },
+      () => setTimeout(this.setPercentage.bind(this), 1000),
+    );
+  }
   render() {
+    const { rotate } = this.state;
+    const style = { ...inner, transform: `rotate(${rotate}deg)` };
     return (
-      <div>Spinning...</div>
+      <div><div style={style} /></div>
     );
   }
 }
