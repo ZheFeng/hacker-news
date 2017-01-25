af9f39ebbc43eacd26d39319b3b08c9150575379
Zhe Feng <zhe@jumiduo.com>
2017-01-25 16:27:44 +1100

    Update server html

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use('/static', express.static('static'));
 
 app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/static/index.html`);
+  res.sendFile(`${__dirname}/static/index.server.html`);
 });
 
 const port = process.env.PORT || 3000;
